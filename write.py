da2952f94da02fd3a0dd5125320a1f8fa51047eb
Kavya Vellanki <praneetha.vellanki@gmail.com>
2023-11-21 11:54:48 -0500

    Write function
    Minor changes made on write function

--- write.py
+++ write.py
@@ -9,7 +9,7 @@ from pkcs1.hazmat.primitives.asymmetric import padding
 
 
 def write_file(username):
-    x = "please send me the file name to perform write operation ";
+    x = "Please send me the file name to perform write operation ";
     client.send(x.encode('utf-8'))
     filename_encrypted = client.recv(1024).decode('utf-8')
     filename = filename_encrypted;
@@ -17,7 +17,7 @@ def write_file(username):
         with open("C:/Users/kavya/OneDrive/Desktop/pcs_proj/" + filename + ".txt", 'r+') as f:
             c.execute("select file_id from files where filename=%s",(filename,))
             file_id=c.fetchone()[0]
-            c.execute("SELECT wr FROM acess_control WHERE username=%s and file_id=%s", (username,file_id))
+            c.execute("SELECT wr FROM access_control WHERE username=%s and file_id=%s", (username,file_id))
             result = c.fetchone()
             if result is not None:
                 write_access=result[0]
@@ -26,7 +26,7 @@ def write_file(username):
                 if (write_access == 1):
                     fcntl.flock(f, fcntl.LOCK_EX)
                     data = f.read()
-                    c.execute("select public_key from acess_control where username=%s and file_id=(select max(file_id) from acess_control where username=%s)",(username, username))
+                    c.execute("select public_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)",(username, username))
                     result = c.fetchone()[0]
                     result = result.replace('-----BEGIN RSA PUBLIC KEY-----\n', '')
                     result = result.replace('\n-----END RSA PUBLIC KEY-----\n', '')
@@ -34,7 +34,7 @@ def write_file(username):
                     public_key_bytes = base64.b64decode(public_key_str)
                     public_key = rsa.import_key(public_key_bytes)
                     cipher = padding.OAEP(mgf=padding.MGF1(algorithm=11289220), algorithm=hashes.SHA256, label=None)
-                    x = "please send me the data to be written ";
+                    x = "Please send me the data to be written ";
                     client.send(x.encode('utf-8'))
                     data_encrypted = client.recv(1024).decode('utf-8')
                     ciphertext = rsa.decrypt(base64.b64decode(data_encrypted), private_key,
@@ -59,10 +59,14 @@ def write_file(username):
                         (username, filename, "write", transaction_time))
                     cnx.commit()
                 else:
-                    write_message = "you don't have permission to write!";
+                    write_message = "You don't have permission to write!";
                     client.send(write_message.encode('utf-8'))
             else:
                 write_access=0
                 print(write_access)
                 client.send(str(write_access).encode('utf-8'))
-                write_message = "You are not authorised to access the file
+                write_message = "You are not authorised to access the file";
+    except Exception as e:
+        write_message = "You are not authorised to access the file or the file does not exist ";
+        client.send(write_message.encode('utf-8'))
+        print('File not found.', e)
