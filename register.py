d354b06935990857da3ac710d21f992f0b09a91e
Vaishu999 <anjani.lingamallu1412@gmail.com>
2023-11-23 13:53:46 -0500

    Update register.py

--- register.py
+++ register.py
@@ -6,12 +6,12 @@ from cryptography.hazmat.primitives import serialization
 def register():
     usr = input("Enter new username: ")
     pwd = input("Enter new password: ")
-    c.execute("SELECT * FROM users WHERE username=%s", (usr,))
-    if c.fetchone() is not None:
+    database_cursor.execute("SELECT * FROM users WHERE username=%s", (usr,))
+    if database_cursor.fetchone() is None:
         print("Invalid Username or Password")
     else:
         pwd_hash = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
-        c.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (usr, pwd_hash))
+        database_cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (usr, pwd_hash))
         cnx.commit()
         print("New user created successfully")
 
@@ -20,6 +20,6 @@ def register():
         public_key = key.public_key().public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.PKCS1)
         private_key = key.private_bytes(serialization.Encoding.PEM, serialization.PrivateFormat.PKCS8,serialization.NoEncryption())
         sql = "INSERT INTO access_control (public_key, private_key, username, re, wr, delet, cre, rest,file_id) VALUES (%s, %s, %s,%s, %s, %s, %s, %s,%s)"
-        val = (public_key.decode('utf-8'), private_key.decode('utf-8'), usr, 1,1,1,1,1,1)
-        c.execute(sql, val)
+        val = (public_key.decode('utf-8'), private_key.decode('utf-8'), usr, 0,0,0,0,0,0)
+        database_cursor.execute(sql, val)
         cnx.commit()
