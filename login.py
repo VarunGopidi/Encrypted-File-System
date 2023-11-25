dc5abe95472e2f1f6ba9493b44bc0ebd384d07f8
challasaiteja <saitejachalla2001@gmail.com>
2023-11-25 11:53:42 -0500

    Updated login with AES

--- login.py
+++ login.py
@@ -13,44 +13,73 @@ from crypto.Hash import SHA256
 from crypto.Signature import PKCS1_v1_5
 from log_activity import log_activity
 
-def login(database_cursor, database_connection, network_client):
+
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
+
+
+def encrypt_path(path, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(path.encode(), AES.block_size))
+    iv = cipher.iv
+    return iv + ct_bytes
+
+def decrypt_path(encrypted_path, key):
+    iv = encrypted_path[:AES.block_size]
+    ct = encrypted_path[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    pt = unpad(cipher.decrypt(ct), AES.block_size)
+    return pt.decode()
+
+
+def login():
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    # Serialize the public and private keys to PEM format
+    public_key_pem = key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    private_key_pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    # Load the public key from the PEM-encoded string
+    rsa_public_key = serialization.load_pem_public_key(public_key_pem, backend=default_backend())
     def get_user_credentials():
-        entered_username = input("Enter user_name: ")
-        entered_password = input("Enter password: ")
+        entered_username = input("Enter the username: ")
+        entered_password = input("Enter the password: ")
         return entered_username, hashlib.sha256(entered_password.encode('utf-8')).hexdigest()
 
     def verify_user_credentials(usr, hashed_password):
-        database_cursor.execute("SELECT password FROM users WHERE username=%s", (user_name,))
+        database_cursor.execute("SELECT password FROM users WHERE username=%s", (usr,))
         stored_user_data = database_cursor.fetchone()
         return stored_user_data and stored_user_data[0] == hashed_password
 
-    def fetch_user_keys(user_name):
-        database_cursor.execute("SELECT public_key, private_key FROM access_control WHERE username=%s ORDER BY file_id DESC LIMIT 1", (user_name,))
-        return database_cursor.fetchone()
+    # def fetch_user_priv_keys(user_name):
+    #     database_cursor.execute("SELECT private_key FROM access_control WHERE username=%s and file_id=(select max(file_id) from access_control where username=%s)", (user_name,))
+    #     result= database_cursor.fetchone()
+    #     #print(result)
+    #     return result[0]
+    # def fetch_user_pub_keys(user_name):
+    #     database_cursor.execute("SELECT public_key FROM access_control WHERE username=%s and file_id=(select max(file_id) from access_control where username=%s)", (user_name,))
+    #     result= database_cursor.fetchone()
+    #     #print(result)
+    #     return result[0]
 
-
-    def get_user_command():
-        while True:
-            try:
-                user_command = int(input("Enter a command (1. Create a File, 2. Read a File, 3. Write to File, 4. Restore a File, 5. Delete a File, 6. Exit from Application): "))
-                if 1 <= user_command <= 6:
-                    return user_command
-                else:
-                    print("Invalid command. Please enter a valid command.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-
-    
-    def log_exit(user_name, timer)
-
-
-
-
-    def process_user_commands(user_name):
-        command_start_time = time.time()
+    def process_user_commands(user_name, command_start_time):
+        x=command_start_time
         log_activity(user_name, "as logged into File System")
+        #y=rsa_private_key
         while True:
-            user_command = get_user_command()
+            user_command = int(input("Enter an operation that you wish to perform (1. CREATE, 2. READ, 3. WRITE, 4. RESTORE, 5. DELETE, 6. EXIT): "))
             if user_command == 6:
                 command_end_time = time.time()
                 msg = f"Total execution time: {command_end_time - command_start_time:.2f} seconds"
@@ -58,14 +87,28 @@ def login(database_cursor, database_connection, network_client):
                 print(msg)
                 log_activity(user_name, "as exited the file system !")
                 log_activity(user_name, msg)
-                
                 break
             else:
-                execute_user_command(user_name, user_command)
+                execute_user_command(user_name, user_command,x)
 
-    def execute_user_command(user_name, command_id):
+    def execute_user_command(username, command_id, command_start_time):
+        x=command_start_time
+        command=command_id
+        database_cursor.execute("select public_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)",(username, username))
+        result = database_cursor.fetchone()[0]
+        result = result.replace('-----BEGIN RSA PUBLIC KEY-----\n', '')
+        result = result.replace('\n-----END RSA PUBLIC KEY-----\n', '')
+        public_key_str = result
+        public_key_bytes = base64.b64decode(public_key_str)
+        public_key = RSA.import_key(public_key_bytes)
+        database_cursor.execute("select private_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)",(username, username))
+        result = database_cursor.fetchone()[0]
+            # result = result.replace('-----BEGIN PRIVATE KEY-----\n', '')
+            # result = result.replace('\n-----END PRIVATE KEY-----\n', '')
+        privateKeyString = result
+        private_key = RSA.import_key(privateKeyString)
         while True:
-                command =get_user_command() # This queery will repeat if the user first selects 1 in process user commands and execute user commands again generates this query
+                #command =get_user_command() # This queery will repeat if the user first selects 1 in process user commands and execute user commands again generates this query
                 if command == 1:
                     message = username + ':create'
                     client.send(message.encode('utf-8'))
@@ -83,6 +126,10 @@ def login(database_cursor, database_connection, network_client):
 
                         set_permissions = input("Do you want to set permissions for other users? (y/n) ")
                         if set_permissions.lower() == 'y':
+                            c.execute("SELECT username FROM users WHERE username != %s GROUP BY username", (username,))
+                            rows = c.fetchall()
+                            for i in rows:
+                                print(i)
                             other_username = input("Enter the username of the user you want to give permissions to: ")
                             # c.execute("select public_key from acess_control where username=%s",(other_username,))
                             # other_user_pub_key = c.fetchone()
@@ -97,7 +144,7 @@ def login(database_cursor, database_connection, network_client):
                                 # file_id = row[0]
                                 # print(row)
                                 print("Enter the permissions you want to grant (read, write, delete, create, restore): ")
-                                print("Enter "1" for yes and "0" for no correspondingly in order")
+                                print("Enter 1 for yes and 0 for no correspondingly in order")
                                 re = int(input())
                                 wr = int(input())
                                 delet = int(input())
@@ -174,10 +221,11 @@ def login(database_cursor, database_connection, network_client):
                     print(sample)
                     filename = input()
                     filename_encrypted = filename.encode('utf-8')
-                    log_activity(username,"{has restored a file %s", filename})
+                    log_activity(username,{"has restored a file %s", filename})
                     client.send(filename_encrypted)
                     data = client.recv(1024)
                     print(data)
+
                 elif command == 5:
                     message = username + ':delete'
                     client.send(message.encode('utf-8'))
