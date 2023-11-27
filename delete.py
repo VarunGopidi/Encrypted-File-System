751c1858dec09707ac0e72c63de167106f0af054
Seetaram_oruganti <orugantiseetaram@gmail.com>
2023-11-27 10:40:52 -0500

    Update delete.py
    added log functionality to delete function

--- delete.py
+++ delete.py
@@ -1,44 +1,39 @@
 def delete_file(username):
-    prompt_message = "Please send me the file name that you want to delete"
-    client.send(prompt_message.encode('utf-8'))
-    filename = client.recv(1024).decode('utf-8')
-
-    src_path = f"/Users/saitejachalla/Desktop/PCS Project/{filename}.txt"
-    dest_path = f"/Users/saitejachalla/Desktop/PCS Project/restore_files/{filename}.txt"
-
+    x = "please send me the file name that you want to delete "
+    client.send(x.encode('utf-8'))
+    filename_encrypted = client.recv(1024).decode('utf-8')
+    filename = filename_encrypted
+    # filepath = os.path.join("Users","dineshgadu", "Desktop","PCS Project", filename,".txt")
     try:
-        # Check if user has delete permissions
-        database_cursor.execute("SELECT delet FROM access_control WHERE username=%s AND file_id=(SELECT file_id FROM files WHERE filename=%s)", (username, filename))
-        access = database_cursor.fetchone()
-
-        if access and access[0] == 1:
-            # Move the file to restore folder instead of deleting
+        # with open("/Users/dineshgadu/Desktop/PCS Project/" + filename + ".txt", 'r') as f:
+    # c.execute("SELECT * FROM transactions WHERE filename=?", (filename,))
+    # if c.fetchone() is
+    # if os.path.exists(filepath):
+        c.execute("select file_id from files where filename=%s", (filename,))
+        file_id = c.fetchone()[0]
+        print(file_id)
+        c.execute("SELECT delet FROM access_control WHERE username=%s and file_id=%s", (username,file_id))
+        access = c.fetchone()
+        # print(access)
+        if (access[0] == 1):
+            src_path = "/Users/saitejachalla/Desktop/PCS Project/" + filename + ".txt"
+            dest_path = "/Users/saitejachalla/Desktop/PCS Project/restore_files/" + filename + ".txt"
             shutil.move(src_path, dest_path)
-
-            # Log the transaction
+            # os.remove("/Users/dineshgadu/Desktop/PCS Project/" + filename + ".txt")
             transaction_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            database_cursor.execute("INSERT INTO transactions (username, file_name, transaction_type, transaction_time) VALUES (%s, %s, %s, %s)", (username, filename, "delete", transaction_time))
+            c.execute("INSERT INTO transactions (username, file_name, transaction_type, transaction_time) VALUES (%s, %s, %s,%s)",(username, filename, "delete", transaction_time))
             cnx.commit()
-
-            # Send success message
-            delete_message = "The file has been deleted successfully!"
+            delete_message = "File deleted!"
             client.send(delete_message.encode('utf-8'))
-
-            # Notify replica servers
-            notify_replica_servers('delete', filename, replica_servers)
+            for replica_server in replica_servers:
+                with socket(AF_INET, SOCK_STREAM) as s:
+                    s.connect(replica_server)
+                    request = ('delete', filename, '')
+                    s.sendall(pickle.dumps(request))
         else:
-            client.send("You do not have the permission to delete!".encode('utf-8'))
-            exit(0)
-
-    except FileNotFoundError:
-        client.send("File not found".encode('utf-8'))
+            delete_message = "You don't have permission to perform delete operation!"
+            client.send(delete_message.encode('utf-8'))
     except Exception as e:
-        client.send("An error occurred".encode('utf-8'))
-        print(f'Error: {e}')
-
-def notify_replica_servers(operation, filename, replica_servers):
-    for server in replica_servers:
-        with socket(AF_INET, SOCK_STREAM) as s:
-            s.connect(server)
-            request = (operation, filename, '')
-            s.sendall(pickle.dumps(request))
+        delete_message = "You are not authorised to access the file or the file does not exist "
+        client.send(delete_message.encode('utf-8'))
+        print('File not found.', e)
