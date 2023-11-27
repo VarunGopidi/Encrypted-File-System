78f4bd2bc5f6597f6f25519151f2f790ff4b40c3
Seetaram_oruganti <orugantiseetaram@gmail.com>
2023-11-27 10:42:18 -0500

    Update create.py
    added log file messages to create file function

--- create.py
+++ create.py
@@ -1,64 +1,58 @@
 def create_file(username):
+    x = "please send me the file name that you wanted to create "
+    client.send(x.encode('utf-8'))
+    filename_encrypted = client.recv(1024).decode('utf-8')
+    filename = filename_encrypted
+    # filepath = "/Users/dineshgadu/Desktop/PCS Project" + filename + ".txt"
+    #
+    # if not os.path.exists("/Users/dineshgadu/Desktop/PCS Project"):
+    #     os.makedirs("/Users/dineshgadu/Desktop/PCS Project")
     try:
-        request_filename = "Please send me the file name that you want to create "
-        client.send(request_filename.encode('utf-8'))
-        filename_encrypted = client.recv(1024).decode('utf-8')
-        filename = filename_encrypted
-
-        database_cursor.execute("SELECT cre FROM access_control WHERE username=%s and file_id=%s", (username, 1))
-        access = database_cursor.fetchone()
-
-        if access[0] == 1:
-            aes_key = get_random_bytes(16)  # AES key for 128-bit encryption
-            # Encrypt the file path
-            encrypted_file_path = encrypt_path("/Users/saitejachalla/Desktop/PCS Project/" + filename + ".txt", aes_key)
-            # When you need to use the file path, decrypt it
-            decrypted_file_path = decrypt_path(encrypted_file_path, aes_key)
-
-            with open(decrypted_file_path, 'w') as f:
-            # file_path = "/Users/saitejachalla/Desktop/PCS Project/" + filename + ".txt"
-            # with open(file_path, 'w') as file:
-                data = "The file has been created successfully!"
+        c.execute("SELECT cre FROM access_control WHERE username=%s and file_id=%s", (username,1))
+        access = c.fetchone()
+        # print(access)
+        if (access[0] == 1):
+            with open("/Users/saitejachalla/Desktop/PCS Project/"+filename+".txt", 'w') as f:
+                data="File created successfully!"
+                # file_id = c.lastrowid
                 client.send(data.encode('utf-8'))
-
                 transaction_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 print(transaction_time)
 
-                sql_insert_file = "INSERT INTO files (filename, owner) VALUES (%s, %s)"
-                val_insert_file = (filename, username)
-                print("Inserting file into the database")
-                database_cursor.execute(sql_insert_file, val_insert_file)
+                sql1 = "INSERT INTO files (filename,owner) VALUES (%s, %s)"
+                val1 = (filename, username)
+                print("inserting...")
+                c.execute(sql1, val1)
+                print("inserted...")
                 cnx.commit()
-                print("The file has been committed into the database!")
-
-                database_cursor.execute("SELECT file_id FROM files WHERE filename=%s", (filename,))
-                file_row = database_cursor.fetchone()
-                file_id = file_row[0]
+                print("commited...")
+                c.execute("SELECT file_id FROM files WHERE filename=%s", (filename,))
+                row = c.fetchone()
+                file_id = row[0]
+                print(file_id)
                 client.send(str(file_id).encode('utf-8'))
                 cnx.commit()
-
-                database_cursor.execute("select public_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)", (username, username))
-                other_user_pub_key = database_cursor.fetchone()[0]
-
-                database_cursor.execute("select private_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)", (username, username))
-                other_user_pri_key = database_cursor.fetchone()[0]
-
-                database_cursor.execute("INSERT INTO access_control (public_key,private_key,username,re,wr,delet,cre,rest,file_id) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (other_user_pub_key, other_user_pri_key, username, 1, 1, 1, 1, 1, file_id))
-
-                sql_insert_transaction = "INSERT INTO transactions (username, file_name, transaction_type, transaction_time) VALUES (%s, %s, %s,%s)"
-                val_insert_transaction = (username, filename, "create", transaction_time)
-                database_cursor.execute(sql_insert_transaction, val_insert_transaction)
+                c.execute("select public_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)",(username, username))
+                other_user_pub_key = c.fetchone()[0]
+                # print(other_user_pub_key);
+                c.execute("select private_key from access_control where username=%s and file_id=(select max(file_id) from access_control where username=%s)",(username, username))
+                other_user_pri_key = c.fetchone()[0]
+                # print(other_user_pri_key);
+                c.execute("INSERT INTO access_control (public_key,private_key,username,re,wr,delet,cre,rest,file_id) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)",(other_user_pub_key, other_user_pri_key, username, 1, 1, 1, 1, 1, file_id));
+                sql = "INSERT INTO transactions (username, file_name, transaction_type, transaction_time) VALUES (%s, %s, %s,%s)"
+                val = (username, filename, "create", transaction_time)
+                c.execute(sql, val)
                 cnx.commit()
-
             for replica_server in replica_servers:
                 with socket(AF_INET, SOCK_STREAM) as s:
                     s.connect(replica_server)
-                    request = ('create', filename, '')
+                    request = ('create', filename,'')
                     s.sendall(pickle.dumps(request))
-
+            #send message to replica
             replica_message = {"filename": filename, "operation": "create"}
-        else:
-            print("You do not have the permission!")
 
+            # replica_socket.close()
+        else:
+            print("You don't have permission!")
     except Exception as e:
-        print("Error in creating file:", e)
+        print("Error creating file:", e)
